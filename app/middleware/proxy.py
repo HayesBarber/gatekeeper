b937b085716ab50b9296bac044c83ee66f708d99
Hayes Barber <barberhayes@gmail.com>
2025-08-01 19:29:27 -0400

    auto commit

--- app/middleware/proxy.py
+++ app/middleware/proxy.py
@@ -2,4 +2,5 @@ from fastapi import Request
 from app.utils.logger import LOGGER
 
 async def proxy_middleware(request: Request, call_next):
-    LOGGER.info(f"[Proxy] Validating request {request.method} {request.url.path}")
+    LOGGER.info(f"[Proxy] Checking for proxy eligibility for {request.method} {request.url.path}")
+    return await call_next(request)
