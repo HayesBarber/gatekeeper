70487971f33616e2b85b9d00b73d8be9e5800d60
Hayes Barber <barberhayes@gmail.com>
2025-08-02 09:56:46 -0400

    add logs

--- app/middleware/blacklist.py
+++ app/middleware/blacklist.py
@@ -5,9 +5,10 @@ from app.utils.logger import LOGGER
 
 async def blacklist_middleware(request: Request, call_next):
     request_path = request.url.path
+    LOGGER.info(f"[Blacklist] Checking if path is black listed {request_path}")
 
     if request_path in settings.blacklisted_paths:
-        LOGGER.info(f"Blocked request to blacklisted path: {request_path}")
+        LOGGER.warn(f"Blocking request to blacklisted path: {request_path}")
         return JSONResponse(status_code=403, content={"detail": "Access to this path is forbidden."})
 
     return await call_next(request)
