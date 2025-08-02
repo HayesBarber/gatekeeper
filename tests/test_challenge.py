55ab7a00af7af475cc8ef525140d8969f5b7daf6
Hayes Barber <barberhayes@gmail.com>
2025-08-02 14:07:46 -0400

    auto commit

--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -2,5 +2,5 @@ from tests.scripts.setup import load_seeded_user
 from pathlib import Path
 
 def test_generate_challenge():
-    client_id, keypair = load_seeded_user(Path(__file__).parent / "seeded_user.json")
+    client_id, keypair = load_seeded_user(Path(__file__).parent / "generated/seeded_user.json")
     assert client_id == "Test_User"
