b9b8df2a201fb843b9d27c5a140968ab3b3ae314
Hayes Barber <barberhayes@gmail.com>
2025-08-02 14:20:19 -0400

    test challenge flow

--- tests/test_challenge.py
+++ tests/test_challenge.py
@@ -1,6 +1,33 @@
+import requests
 from tests.scripts.setup import load_seeded_user
 from pathlib import Path
+from curveauth.signatures import sign_message
 
-def test_generate_challenge():
+def test_challenge_flow():
     client_id, keypair = load_seeded_user(Path(__file__).parent / "generated/seeded_user.json")
-    assert client_id == "Test_User"
+
+    resp = requests.post("http://localhost:8000/challenge",
+        json={"client_id": client_id}, 
+        headers={"User-Agent": "test-user-agent"}
+    )
+    assert resp.status_code == 200
+    challenge = resp.json()["challenge"]
+    challenge_id = resp.json()["challenge_id"]
+
+    signature = sign_message(challenge, keypair.private_pem().decode("utf-8"))
+
+    verify_resp = requests.post("http://localhost:8000/challenge/verify", json={
+        "client_id": client_id,
+        "challenge_id": challenge_id,
+        "signature": signature
+    }, headers={"User-Agent": "test-user-agent"})
+    assert verify_resp.status_code == 200
+    api_key = verify_resp.json()["api_key"]
+    assert api_key
+
+    proxy_resp = requests.get("http://localhost:8000/proxy/echo", headers={
+        "x-api-key": api_key,
+        "x-requestor-id": client_id,
+        "User-Agent": "test-user-agent"
+    })
+    assert proxy_resp.status_code == 200
