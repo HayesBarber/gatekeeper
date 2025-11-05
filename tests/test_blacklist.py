e3736db795ff94a2f5afb9096b6741828008ac88
Hayes Barber <barberhayes@gmail.com>
2025-11-04 20:48:55 -0500

    remove prints

--- tests/test_blacklist.py
+++ tests/test_blacklist.py
@@ -4,8 +4,6 @@ import requests
 def test_blacklist_blocked_with_proxy():
     url = "http://localhost:8000/proxy/blocked"
     response = requests.get(url)
-    print("Status Code:", response.status_code)
-    print("Response Body:", response.json())
     assert response.status_code == 403
     assert response.json()["detail"] == "Access to this path is forbidden."
 
@@ -13,7 +11,5 @@ def test_blacklist_blocked_with_proxy():
 def test_blacklist_blocked():
     url = "http://localhost:8000/blocked"
     response = requests.get(url)
-    print("Status Code:", response.status_code)
-    print("Response Body:", response.json())
     assert response.status_code == 403
     assert response.json()["detail"] == "Access to this path is forbidden."
