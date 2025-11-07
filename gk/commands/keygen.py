633f68b97fbcda7e591b08037c99d48040898364
Hayes Barber <barberhayes@gmail.com>
2025-11-07 17:01:26 -0500

    init command file

---
+++ gk/commands/keygen.py
@@ -0,0 +1,12 @@
+from rich.console import Console
+
+
+def add_subparser(subparsers):
+    parser = subparsers.add_parser("keygen", help="Generate a ECC keypair")
+    parser.add_argument("instance", type=str, help="The gatekeeper instanse base URL")
+    parser.set_defaults(handler=handle)
+    return parser
+
+
+def handle(args, console: Console):
+    pass
