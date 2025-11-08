c394bc8b2607982057ad034964039dc12efce0ed
Hayes Barber <barberhayes@gmail.com>
2025-11-07 21:45:32 -0500

    adjusting tmp data fixture

--- tests/gk/commands/test_add.py
+++ tests/gk/commands/test_add.py
@@ -1,7 +1,7 @@
 import pytest
 from gk.commands import add
 from gk.models.gk_instance import GkInstance, GkInstances
-from gk.storage import StorageKey
+from gk.storage import StorageKey, load_model, save_model
 
 
 @pytest.mark.parametrize(
@@ -11,12 +11,7 @@ from gk.storage import StorageKey
         (["http://another.com", "test2", "x-client-id", "x-api-key", "n"], False),
     ],
 )
-def test_add_new_instance(console, tmp_storage, inputs, expected_active, monkeypatch):
-    load_model, save_model, storage = tmp_storage
-
-    monkeypatch.setattr(add, "load_model", load_model)
-    monkeypatch.setattr(add, "save_model", save_model)
-
+def test_add_new_instance(console, inputs, expected_active, monkeypatch):
     # Patch console.input to return items from inputs list
     input_iter = iter(inputs)
     monkeypatch.setattr(console, "input", lambda prompt="": next(input_iter))
@@ -24,25 +19,21 @@ def test_add_new_instance(console, tmp_storage, inputs, expected_active, monkeyp
     args = type("Args", (), {})()
     add.handle(args, console)
 
-    instances = storage[StorageKey.INSTANCES].instances
+    instances = load_model(StorageKey.INSTANCES).instances
     assert len(instances) == 1
     assert instances[0].base_url == inputs[0]
     assert instances[0].active == expected_active
 
 
-def test_add_overwrite_instance(console, tmp_storage, monkeypatch):
-    load_model, save_model, storage = tmp_storage
-
+def test_add_overwrite_instance(console, monkeypatch):
     # pre-populate storage with an instance
     existing = GkInstance(
         base_url="http://existing.com",
         client_id="test",
         active=False,
     )
-    storage[StorageKey.INSTANCES] = GkInstances(instances=[existing])
 
-    monkeypatch.setattr(add, "load_model", load_model)
-    monkeypatch.setattr(add, "save_model", save_model)
+    save_model(StorageKey.INSTANCES, GkInstances(instances=[existing]))
 
     inputs = ["http://existing.com", "y", "hello", "x-api-key", "x-client-id", "y"]
     input_iter = iter(inputs)
@@ -51,7 +42,7 @@ def test_add_overwrite_instance(console, tmp_storage, monkeypatch):
     args = type("Args", (), {})()
     add.handle(args, console)
 
-    instances = storage[StorageKey.INSTANCES].instances
+    instances = load_model(StorageKey.INSTANCES).instances
     assert len(instances) == 1
     assert instances[0].base_url == "http://existing.com"
     assert instances[0].active is True
