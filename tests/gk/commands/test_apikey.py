5c5866a5e4fedb0094e5486ebd476988f2909e03
Hayes Barber <barberhayes@gmail.com>
2025-11-08 16:05:45 -0500

    starting apikey tests

---
+++ tests/gk/commands/test_apikey.py
@@ -0,0 +1,35 @@
+from gk.storage import StorageKey, save_model
+from gk.models.gk_instance import GkInstances, GkInstance
+from gk.commands import apikey
+import pytest
+
+
+def test_apikey_no_instances(console):
+    save_model(StorageKey.INSTANCES, GkInstances(instances=[]))
+
+    args = type("Args", (), {})()
+    args.instance = None
+    with pytest.raises(SystemExit):
+        apikey.handle(args, console)
+
+    output = console.export_text()
+    assert "Instance not found" in output
+
+
+def test_apikey_invalid_instance(console):
+    instances = [
+        GkInstance(
+            base_url="http://one.com",
+            client_id="test1",
+            active=False,
+        ),
+    ]
+    save_model(StorageKey.INSTANCES, GkInstances(instances=instances))
+
+    args = type("Args", (), {})()
+    args.instance = "invalid"
+    with pytest.raises(SystemExit):
+        apikey.handle(args, console)
+
+    output = console.export_text()
+    assert "Instance not found" in output
