6ce12bff729161986e1970a611d102eda749067c
Hayes Barber <barberhayes@gmail.com>
2025-11-08 12:02:42 -0500

    fix warning

--- app/models/challenge_verification_response.py
+++ app/models/challenge_verification_response.py
@@ -11,7 +11,7 @@ class ChallengeVerificationResponse(BaseModel):
     class Config:
         json_schema_extra = {
             "example": {
-                "api_key": "sk_live_4f9e2c3b8f7a45c9a07cbd22f41c5ef7",
+                "api_key": "dummy_key",
                 "expires_at": "2025-08-31T23:59:59Z",
             }
         }
