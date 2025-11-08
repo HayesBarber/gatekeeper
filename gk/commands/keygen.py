27cead4ed1857429d6e53ff5ae053e932da8c742
Hayes Barber <barberhayes@gmail.com>
2025-11-07 19:04:12 -0500

    generate keypair for instance

--- gk/commands/keygen.py
+++ gk/commands/keygen.py
@@ -1,5 +1,7 @@
 from rich.console import Console
 from curveauth.keys import ECCKeyPair
+from gk.models.gk_instance import GkInstance
+from gk.models.gk_keypair import GkKeyPair
 
 
 def add_subparser(subparsers):
@@ -10,4 +12,17 @@ def add_subparser(subparsers):
 
 
 def handle(args, console: Console):
+    pass
+
+
+def generate_keypair_for_instance(instance: GkInstance) -> GkKeyPair:
+    instance_base_url = instance.base_url
     keypair = ECCKeyPair.generate()
+    public_key = keypair.public_pem()
+    private_key = keypair.private_pem()
+
+    return GkKeyPair(
+        instance_base_url,
+        public_key,
+        private_key,
+    )
