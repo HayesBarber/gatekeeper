7a095d0a5747d328e4ccc4187dcccdcce56b6232
Hayes Barber <barberhayes@gmail.com>
2025-11-07 20:04:09 -0500

    check if instance exists

--- gk/commands/add.py
+++ gk/commands/add.py
@@ -2,6 +2,7 @@ from rich.console import Console
 from gk.models.gk_instance import GkInstance, GkInstances
 from gk.storage import StorageKey, load_model, save_model
 from gk.commands import keygen
+import sys
 
 
 def add_subparser(subparsers):
@@ -38,22 +39,34 @@ def handle(args, console: Console):
         kwargs["client_id_header"] = client_id_header
 
     instance = GkInstance(**kwargs)
-
     instances_model: GkInstances = load_model(StorageKey.INSTANCES)
+
+    exists = instance_exists(instance, instances_model)
+    if exists:
+        should_continue = console.input("Instance exists, overwrite? y/n: ")
+        if not should_continue.strip().lower().startswith("y"):
+            sys.exit(1)
+
     for i, existing in enumerate(instances_model.instances):
         if existing.base_url == instance.base_url:
             instances_model.instances[i] = instance
             save_model(StorageKey.INSTANCES, instances_model)
-            console.print(
-                f"Overwrote instance '{instance.base_url}' (active={instance.active})"
-            )
             break
     else:
         instances_model.instances.append(instance)
         save_model(StorageKey.INSTANCES, instances_model)
-        console.print(
-            f"Added instance '{instance.base_url}' (active={instance.active})"
-        )
+
+    console.print_json(instance.model_dump_json())
 
     args.instance = instance.base_url
     keygen.handle(args, console)
+
+
+def instance_exists(
+    instance: GkInstance,
+    instances_model: GkInstances,
+) -> bool:
+    for existing in instances_model.instances:
+        if existing.base_url == instance.base_url:
+            return True
+    return False
