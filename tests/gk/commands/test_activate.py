76f0d690d6b52c05ddb1f2d505a7d2e938dc8af5
Hayes Barber <barberhayes@gmail.com>
2025-11-09 16:02:56 -0500

    test activate instance already active

--- tests/gk/commands/test_activate.py
+++ tests/gk/commands/test_activate.py
@@ -33,3 +33,34 @@ def test_activate_happy_path(console):
 
     assert one and one.active
     assert two and not two.active
+
+
+def test_activate_already_active(console):
+    instances = [
+        GkInstance(
+            base_url="http://one.com",
+            client_id="test1",
+            active=True,
+        ),
+        GkInstance(
+            base_url="http://two.com",
+            client_id="test2",
+            active=False,
+        ),
+    ]
+    save_model(StorageKey.INSTANCES, GkInstances(instances=instances))
+
+    args = type("Args", (), {})()
+    args.base_url = "http://one.com"
+
+    activate.handle(args, console)
+    output = console.export_text()
+
+    assert "Instance already active: http://one.com" in output
+    stored = load_model(StorageKey.INSTANCES).instances
+
+    one = next(i for i in stored if i.base_url == "http://one.com")
+    two = next(i for i in stored if i.base_url == "http://two.com")
+
+    assert one and one.active
+    assert two and not two.active
