537679ea2e75d7047063f413fbcb962c93264aae
Hayes Barber <barberhayes@gmail.com>
2025-11-08 19:06:59 -0500

    edit cli.py

--- gk/cli.py
+++ gk/cli.py
@@ -1,18 +1,16 @@
 import argparse
-from gk.commands import add, list_, keygen, apikey, proxy
+import importlib
+import pkgutil
+import gk.commands
 
 
 def build_parser() -> argparse.ArgumentParser:
-    parser = argparse.ArgumentParser(prog="srl")
+    parser = argparse.ArgumentParser(prog="gk")
     subparsers = parser.add_subparsers(dest="command")
 
-    for element in [
-        add,
-        list_,
-        keygen,
-        apikey,
-        proxy,
-    ]:
-        element.add_subparser(subparsers)
+    for _, module_name, _ in pkgutil.iter_modules(gk.commands.__path__):
+        module = importlib.import_module(f"gk.commands.{module_name}")
+        if hasattr(module, "add_subparser"):
+            module.add_subparser(subparsers)
 
     return parser
