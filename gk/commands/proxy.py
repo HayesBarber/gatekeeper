e3ef5bc2bf6a0d4424088c1460638e458af2301d
Hayes Barber <barberhayes@gmail.com>
2025-11-08 19:08:47 -0500

    add instance flag

--- gk/commands/proxy.py
+++ gk/commands/proxy.py
@@ -8,7 +8,14 @@ def add_subparser(subparsers):
     )
     parser.add_argument("method", choices=["GET", "POST", "PUT", "DELETE", "PATCH"])
     parser.add_argument("path", help="API path (relative to instance base URL)")
-    parser.add_argument("--body", dest="body", help="JSON body or @file.json")
+    parser.add_argument("-b", "--body", dest="body", help="JSON body or @file.json")
+    parser.add_argument(
+        "-i",
+        "--instance",
+        dest="instance",
+        type=str,
+        help="Base URL of the Gatekeeper instance. Active instance is used if not specified",
+    )
     parser.set_defaults(handler=handle)
     return parser
 
