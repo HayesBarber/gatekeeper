b5054e447dac9030497844cba3325b06a9de174d
Hayes Barber <barberhayes@gmail.com>
2025-11-09 16:01:33 -0500

    test activate happy path

---
+++ tests/gk/commands/test_activate.py
@@ -0,0 +1,35 @@
+from gk.commands import activate
+from gk.models.gk_instance import GkInstances, GkInstance
+from gk.storage import StorageKey, save_model, load_model
+
+
+def test_activate_happy_path(console):
+    instances = [
+        GkInstance(
+            base_url="http://one.com",
+            client_id="test1",
+            active=False,
+        ),
+        GkInstance(
+            base_url="http://two.com",
+            client_id="test2",
+            active=True,
+        ),
+    ]
+    save_model(StorageKey.INSTANCES, GkInstances(instances=instances))
+
+    args = type("Args", (), {})()
+    args.base_url = "http://one.com"
+
+    activate.handle(args, console)
+    output = console.export_text()
+
+    assert "Activated instance: http://one.com" in output
+    stored = load_model(StorageKey.INSTANCES).instances
+    assert len(stored) == 2
+
+    one = next(i for i in stored if i.base_url == "http://one.com")
+    two = next(i for i in stored if i.base_url == "http://two.com")
+
+    assert one and one.active
+    assert two and not two.active
