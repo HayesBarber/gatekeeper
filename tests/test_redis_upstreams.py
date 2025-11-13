4a27f1e63c4110d7a3597ed4fb086a6511a3ffef
Hayes Barber <barberhayes@gmail.com>
2025-11-12 21:30:56 -0500

    testing redis upstreams

--- tests/test_redis_upstreams.py
+++ tests/test_redis_upstreams.py
@@ -11,3 +11,35 @@ def clean_redis():
         keys = redis_client._redis.keys(pattern)
         if keys:
             redis_client._redis.delete(*keys)
+
+
+def test_load_redis_upstreams_returns_empty_when_no_data():
+    settings = Settings()
+    upstreams = settings.load_redis_upstreams()
+    assert isinstance(upstreams, dict)
+    assert len(upstreams) == 0
+
+
+def test_redis_upstream_overrides_static():
+    static_path = "/static-path"
+    static_upstream = "http://static-upstream"
+    settings = Settings(
+        upstreams={
+            static_path: static_upstream,
+        }
+    )
+    assert len(settings.upstreams) == 1
+    assert settings.upstreams.get(static_path) == static_upstream
+
+    redis_upstream = "http://redis-upstream"
+    mapping = UpstreamMapping(
+        prefix=static_path,
+        base_url=redis_upstream,
+    )
+    redis_client.set_model(Namespace.UPSTREAMS, static_path, mapping)
+
+    redis_upstreams = settings.load_redis_upstreams()
+    assert redis_upstreams.get(static_path) == redis_upstream
+    combined = settings.get_combined_upstreams()
+    assert len(combined) == 1
+    assert combined.get(static_path) == redis_upstream
