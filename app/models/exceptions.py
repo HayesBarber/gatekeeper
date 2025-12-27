20d6c911b8ccd1753c50b6f39520b373048f45cb
HayesBarber <barberhayes@gmail.com>
2025-12-27 14:17:39 -0500

    purge project and dart_frog create

--- app/models/exceptions.py
+++
@@ -1,9 +0,0 @@
-class ClientNotFound(Exception):
-    def __init__(self, client_id: str):
-        self.client_id = client_id
-        super().__init__(f"Client '{client_id}' not found.")
-
-class ChallengeNotVerified(Exception):
-    def __init__(self, client_id: str):
-        self.client_id = client_id
-        super().__init__(f"Challenge for client '{client_id}' could not be verified.")
--- app/models/upstream.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-
-class UpstreamMapping(BaseModel):
-    prefix: str
-    base_url: str
