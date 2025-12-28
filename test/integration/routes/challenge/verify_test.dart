f1e505fa9db89015e8945ec3de9e85ce71d3b1a5
HayesBarber <barberhayes@gmail.com>
2025-12-28 12:46:04 -0500

    auto commit

--- test/integration/routes/challenge/verify_test.dart
+++ test/integration/routes/challenge/verify_test.dart
@@ -15,22 +15,31 @@ void main() {
       expect(res.body, equals('Unauthorized'));
     });
 
-    test('returns 401 if challenge not found for client', () async {
+    test('returns 401 if no public key found for client', () async {
       final res = await http.post(
         TestEnv.apiUri('/challenge/verify'),
         headers: {
-          'x-requestor-id': TestEnv.clientId,
+          'x-requestor-id': 'eb893043-d510-460b-ace7-c9b9057d16d9',
         },
       );
       expect(res.statusCode, equals(HttpStatus.unauthorized));
       expect(res.body, equals('Unauthorized'));
     });
 
+    test('returns 404 if challenge not found for client', () async {
+      final res = await http.post(
+        TestEnv.apiUri('/challenge/verify'),
+        headers: {
+          'x-requestor-id': TestEnv.clientId,
+        },
+      );
+      expect(res.statusCode, equals(HttpStatus.notFound));
+      expect(res.body, equals('No challenge found'));
+    });
+
     test('returns 400 if challenge ID does not match provided', () async {});
 
     test('returns 400 if challenge is expired', () async {});
-
-    test('returns 401 if no public key found for client', () async {});
 
     test('returns 403 for invalid signature', () async {});
 
