6cf64a2c6462745582fa98632513691096a410cc
HayesBarber <barberhayes@gmail.com>
2025-12-28 12:36:20 -0500

    stub out tests

--- test/integration/routes/challenge/verify_test.dart
+++ test/integration/routes/challenge/verify_test.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:http/http.dart' as http;
 import 'package:test/test.dart';
 
@@ -5,12 +7,23 @@ import '../../util/test_env.dart';
 
 void main() {
   group('POST /challenge/verify', () {
-    test('responds with a 200 and greeting.', () async {
-      final res = await http.get(
+    test('returns 401 if client ID header is missing', () async {
+      final res = await http.post(
         TestEnv.apiUri('/challenge/verify'),
       );
-      expect(res.statusCode, 200);
-      expect(res.body, 'This is a new route!');
+      expect(res.statusCode, equals(HttpStatus.unauthorized));
     });
+
+    test('returns 401 if challenge not found for client', () async {});
+
+    test('returns 400 if challenge ID does not match provided', () async {});
+
+    test('returns 400 if challenge is expired', () async {});
+
+    test('returns 401 if no public key found for client', () async {});
+
+    test('returns 403 for invalid signature', () async {});
+
+    test('returns 200 and api key for valid signature', () async {});
   });
 }
