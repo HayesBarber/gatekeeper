a49b1a288a882794351261533d20d48ab96cfc22
HayesBarber <barberhayes@gmail.com>
2025-12-28 15:32:19 -0500

    start unit tests for challenge verify

--- test/unit/routes/challenge/verify_test.dart
+++ test/unit/routes/challenge/verify_test.dart
@@ -1,5 +1,47 @@
+import 'dart:io';
+
+import 'package:dart_frog/dart_frog.dart';
+import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
+
+import '../../../../routes/challenge/verify.dart' as route;
+
+class _MockRequestContext extends Mock implements RequestContext {}
+
+class _MockRequest extends Mock implements Request {}
 
 void main() {
   group('POST /challenge/verify', () {});
+
+  group('non-POST methods', () {
+    late Request request;
+    late RequestContext context;
+
+    setUp(() {
+      request = _MockRequest();
+      context = _MockRequestContext();
+
+      when(() => context.request).thenReturn(request);
+    });
+
+    final methods = <HttpMethod>[
+      HttpMethod.get,
+      HttpMethod.put,
+      HttpMethod.patch,
+      HttpMethod.delete,
+      HttpMethod.head,
+      HttpMethod.options,
+    ];
+
+    for (final method in methods) {
+      test('${method.name.toUpperCase()} returns 405', () async {
+        when(() => request.method).thenReturn(method);
+        when(() => request.headers).thenReturn({});
+
+        final response = await route.onRequest(context);
+
+        expect(response.statusCode, equals(HttpStatus.methodNotAllowed));
+      });
+    }
+  });
 }
