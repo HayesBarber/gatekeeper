af5765091afaeb02168c5a60fb2188076c59ed92
HayesBarber <barberhayes@gmail.com>
2025-12-28 18:21:09 -0500

    add unit tests for parsiing config from yaml

---
+++ test/unit/lib/config/yaml_config_service_test.dart
@@ -0,0 +1,33 @@
+import 'package:gatekeeper/config/yaml_config_service.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('YamlConfigService', () {
+    test('loads config from file', () async {
+      const fullYaml = './test/unit/lib/config/test_yaml/full.yaml';
+      final service = await YamlConfigService.load(path: fullYaml);
+      final config = service.config;
+
+      expect(config.redisHost, '192.168.1.10');
+      expect(config.clientIdHeader, 'x-client-id');
+    });
+
+    test('loads default app config on invalid path', () async {
+      const invalidPath = './test/unit/lib/config/test_yaml/missing.yaml';
+      final service = await YamlConfigService.load(path: invalidPath);
+      final config = service.config;
+
+      expect(config.redisHost, '127.0.0.1');
+      expect(config.clientIdHeader, 'x-requestor-id');
+    });
+
+    test('loads config partial yaml', () async {
+      const partialYaml = './test/unit/lib/config/test_yaml/partial.yaml';
+      final service = await YamlConfigService.load(path: partialYaml);
+      final config = service.config;
+
+      expect(config.redisHost, '127.0.0.1');
+      expect(config.clientIdHeader, 'x-custom-id');
+    });
+  });
+}
