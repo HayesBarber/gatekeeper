4cce6b4f1abd38c738cf786c39c590710bc43800
HayesBarber <barberhayes@gmail.com>
2025-12-28 17:49:47 -0500

    remove required headers

--- lib/middleware/required_headers.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dart_frog/dart_frog.dart';
-
-Middleware requiredHeaders() {
-  return (Handler handler) {
-    return (RequestContext context) async {
-      return handler(context);
-    };
-  };
-}
--- routes/_middleware.dart
+++ routes/_middleware.dart
@@ -2,7 +2,6 @@ import 'package:curveauth_dart/curveauth_dart.dart';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:gatekeeper/config/config_service.dart';
 import 'package:gatekeeper/config/yaml_config_service.dart';
-import 'package:gatekeeper/middleware/required_headers.dart';
 import 'package:gatekeeper/redis/redis_client.dart';
 import 'package:gatekeeper/redis/shorebird_redis_client.dart';
 import 'package:gatekeeper/types/signature_verifier.dart';
@@ -12,7 +11,6 @@ final _config = YamlConfigService.instance();
 
 Handler middleware(Handler handler) {
   return handler
-      .use(requiredHeaders())
       .use(provider<ConfigService>((_) => _config))
       .use(provider<SignatureVerifier>((_) => VerifySignature.verifySignature))
       .use(provider<RedisClientBase>((_) => _redis));
