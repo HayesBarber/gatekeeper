50e3debd2599e9f84b82c13d1e564c81337ebba7
HayesBarber <barberhayes@gmail.com>
2025-12-28 13:42:44 -0500

    create helper for getting challenge

--- test/integration/routes/challenge/verify_test.dart
+++ test/integration/routes/challenge/verify_test.dart
@@ -9,6 +9,22 @@ import '../../util/test_env.dart';
 
 void main() {
   group('POST /challenge/verify', () {
+    Future<ChallengeResponse> getChallenge() async {
+      final challengeRes = await http.post(
+        TestEnv.apiUri('/challenge'),
+        headers: {
+          'x-requestor-id': TestEnv.clientId,
+        },
+      );
+      expect(challengeRes.statusCode, equals(HttpStatus.ok));
+      expect(challengeRes.body, isNotEmpty);
+      final challenge = ChallengeResponse.decode(challengeRes.body);
+      expect(challenge.challengeId, isNotEmpty);
+      expect(challenge.challenge, isNotEmpty);
+      expect(challenge.expiresAt, isNotNull);
+      return challenge;
+    }
+
     test('returns 401 if client ID header is missing', () async {
       final res = await http.post(
         TestEnv.apiUri('/challenge/verify'),
@@ -44,19 +60,7 @@ void main() {
     });
 
     test('returns 400 if challenge ID does not match provided', () async {
-      final challengeRes = await http.post(
-        TestEnv.apiUri('/challenge'),
-        headers: {
-          'x-requestor-id': TestEnv.clientId,
-        },
-      );
-      expect(challengeRes.statusCode, equals(HttpStatus.ok));
-      expect(challengeRes.body, isNotEmpty);
-      final challenge = ChallengeResponse.decode(challengeRes.body);
-      expect(challenge.challengeId, isNotEmpty);
-      expect(challenge.challenge, isNotEmpty);
-      expect(challenge.expiresAt, isNotNull);
-
+      final challenge = await getChallenge();
       final res = await http.post(
         TestEnv.apiUri('/challenge/verify'),
         headers: {
@@ -84,12 +88,16 @@ void main() {
     });
 
     test('returns 403 for invalid signature', () async {
+      final challenge = await getChallenge();
       final res = await http.post(
         TestEnv.apiUri('/challenge/verify'),
         headers: {
           'x-requestor-id': TestEnv.clientId,
         },
-        body: 'todo',
+        body: ChallengeVerificationRequest(
+          challengeId: challenge.challengeId,
+          signature: 'invalid',
+        ).encode(),
       );
       expect(res.statusCode, equals(HttpStatus.forbidden));
       expect(res.body, equals('Invalid signature'));
