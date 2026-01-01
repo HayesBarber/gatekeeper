5348e36f10b7bda7c015fd4fb8d076880ff8b178
HayesBarber <barberhayes@gmail.com>
2026-01-01 15:36:23 -0500

    check for github subdomain

--- lib/middleware/github_webhook.dart
+++ lib/middleware/github_webhook.dart
@@ -1,8 +1,14 @@
 import 'package:dart_frog/dart_frog.dart';
+import 'package:gatekeeper/constants/subdomains.dart';
+import 'package:gatekeeper/util/subdomain.dart';
 
 Middleware githubWebhook() {
   return (handler) {
     return (context) async {
+      final subdomain = Subdomain.fromUri(context.request.uri);
+      if (subdomain != github) {
+        return handler(context);
+      }
       return handler(context);
     };
   };
