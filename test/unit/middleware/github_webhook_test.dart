c0e771f64556cef8a28972bfcf5fd993a55fde1e
HayesBarber <barberhayes@gmail.com>
2026-01-01 21:04:56 -0500

    test setup

--- test/unit/middleware/github_webhook_test.dart
+++ test/unit/middleware/github_webhook_test.dart
@@ -1,6 +1,7 @@
 import 'package:dart_frog/dart_frog.dart';
 import 'package:gatekeeper/config/config_service.dart';
 import 'package:gatekeeper/middleware/github_webhook.dart';
+import 'package:gatekeeper/util/forward_to_upstream.dart';
 import 'package:mocktail/mocktail.dart';
 import 'package:test/test.dart';
 
@@ -10,23 +11,31 @@ class _MockRequest extends Mock implements Request {}
 
 class _MockConfigService extends Mock implements ConfigService {}
 
+class _MockForward extends Mock implements Forward {}
+
 void main() {
   group('GitHub webhook middleware', () {
     late _MockRequestContext context;
     late _MockRequest request;
     late _MockConfigService configService;
+    late _MockForward forward;
+
+    const responseBody = 'hello world';
 
     Response handler(context) {
-      return Response(body: 'hello world');
+      return Response(body: responseBody);
     }
 
     setUp(() {
       context = _MockRequestContext();
       request = _MockRequest();
       configService = _MockConfigService();
+      forward = _MockForward();
 
       when(() => context.request).thenReturn(request);
       when(() => request.uri).thenReturn(Uri.parse('http://localhost/'));
+      when(() => context.read<ConfigService>()).thenReturn(configService);
+      when(() => context.read<Forward>()).thenReturn(forward);
     });
 
     test('Falls through when no match', () async {
