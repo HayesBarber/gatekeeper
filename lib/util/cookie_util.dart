aa7d58189407ae4a6e21d452b7adaa10c57caad8
Hayes Barber <barberhayes@gmail.com>
2026-01-05 18:33:16 -0500

    add cookie util

---
+++ lib/util/cookie_util.dart
@@ -0,0 +1,61 @@
+import 'dart:io';
+
+class CookieUtil {
+  static String buildSetCookieHeader(
+    String name,
+    String value, {
+    DateTime? expires,
+    String? domain,
+    String? path,
+    int? maxAge,
+    bool httpOnly = true,
+    bool secure = true,
+    String sameSite = 'Lax',
+  }) {
+    final cookieParts = <String>['$name=$value'];
+
+    if (expires != null) {
+      cookieParts.add('Expires=${_formatExpires(expires)}');
+    }
+
+    if (domain != null) {
+      cookieParts.add('Domain=$domain');
+    }
+
+    if (path != null) {
+      cookieParts.add('Path=$path');
+    }
+
+    if (maxAge != null) {
+      cookieParts.add('Max-Age=$maxAge');
+    }
+
+    if (httpOnly) {
+      cookieParts.add('HttpOnly');
+    }
+
+    if (secure) {
+      cookieParts.add('Secure');
+    }
+
+    cookieParts.add('SameSite=$sameSite');
+
+    return cookieParts.join('; ');
+  }
+
+  static String buildSecureCookie(
+    String name,
+    String value, {
+    DateTime? expires,
+  }) {
+    return buildSetCookieHeader(
+      name,
+      value,
+      expires: expires,
+    );
+  }
+
+  static String _formatExpires(DateTime expires) {
+    return HttpDate.format(expires);
+  }
+}
