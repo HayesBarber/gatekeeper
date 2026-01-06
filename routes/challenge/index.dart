1d6372cd3df15fb1fe163ddfb8fec6d2f1ecb41b
Hayes Barber <barberhayes@gmail.com>
2026-01-06 16:49:56 -0500

    update challenge response

--- routes/challenge/index.dart
+++ routes/challenge/index.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'package:dart_frog/dart_frog.dart';
 import 'package:gatekeeper/dto/challenge_response.dart';
 import 'package:gatekeeper/logging/wide_event.dart' as we;
-import 'package:gatekeeper/middleware/client_id_provider.dart';
 import 'package:gatekeeper/redis/redis_client.dart';
 import 'package:gatekeeper/util/extensions.dart';
 
@@ -15,35 +14,16 @@ Future<Response> onRequest(RequestContext context) {
 }
 
 Future<Response> _onPost(RequestContext context) async {
-  final clientId = context.read<ClientIdContext>().clientId;
-
-  if (clientId == null) {
-    return Response(
-      statusCode: HttpStatus.unauthorized,
-    );
-  }
-
   final eventBuilder = context.read<we.WideEvent>();
   final start = DateTime.now();
 
   final redis = context.read<RedisClientBase>();
-  final publicKey = await redis.get(ns: Namespace.users, key: clientId);
-
-  if (publicKey == null) {
-    eventBuilder.challenge = we.ChallengeContext(
-      operationDurationMs: DateTime.now().since(start),
-      publicKeyPresent: false,
-    );
-    return Response(
-      statusCode: HttpStatus.unauthorized,
-    );
-  }
 
   final challenge = ChallengeResponse.random();
 
   await redis.set(
     ns: Namespace.challenges,
-    key: clientId,
+    key: challenge.challengeId,
     value: challenge.encode(),
   );
 
