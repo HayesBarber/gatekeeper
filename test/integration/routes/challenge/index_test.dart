2574448c127dcb49d10477b4079d82f1286c2b3c
Hayes Barber <barberhayes@gmail.com>
2026-01-06 18:44:48 -0500

    fix tests in test/integration/routes/challenge/index_test.dart

--- test/integration/routes/challenge/index_test.dart
+++ test/integration/routes/challenge/index_test.dart
@@ -9,24 +9,7 @@ import '../../util/test_env.dart';
 
 void main() {
   group('POST /challenge', () {
-    test('returns 401 if client ID header is missing', () async {
-      final res = await http.post(
-        TestEnv.apiUri('/challenge'),
-      );
-      expect(res.statusCode, equals(HttpStatus.unauthorized));
-    });
-
-    test('returns 401 if user not found in Redis', () async {
-      final res = await http.post(
-        TestEnv.apiUri('/challenge'),
-        headers: {
-          headerRequestorId: '7a860417-3b7d-4777-840b-62f0a57d4353',
-        },
-      );
-      expect(res.statusCode, equals(HttpStatus.unauthorized));
-    });
-
-    test('returns 200 and challenge if user exists', () async {
+    test('returns 200 and challenge', () async {
       final res = await http.post(
         TestEnv.apiUri('/challenge'),
         headers: {
