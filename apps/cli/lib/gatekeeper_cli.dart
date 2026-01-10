712976babda50aafbfc9cef0c3df34c92ab3ae04
Hayes Barber <barberhayes@gmail.com>
2026-01-10 13:57:23 -0500

    init package

---
+++ apps/cli/lib/gatekeeper_cli.dart
@@ -0,0 +1,10 @@
+/// gatekeeper_cli, gatekeeper cli
+///
+/// ```sh
+/// # activate gatekeeper_cli
+/// dart pub global activate gatekeeper_cli
+///
+/// # see usage
+/// gk --help
+/// ```
+library;
