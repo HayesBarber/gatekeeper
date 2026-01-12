13f2aedce8a772df8ef4715939cc392776b6e2aa
HayesBarber <barberhayes@gmail.com>
2026-01-11 21:55:44 -0500

    auto commit

--- apps/cli/lib/src/services/registery.dart
+++ apps/cli/lib/src/services/registery.dart
@@ -1,6 +1,11 @@
 import 'package:gatekeeper_cli/src/services/config_service.dart';
 
 class Registery {
+  Registery._();
+  static final _instance = Registery._();
+
+  static Registery get I => _instance;
+
   ConfigService? _configService;
   ConfigService get configService {
     _configService ??= ConfigService();
