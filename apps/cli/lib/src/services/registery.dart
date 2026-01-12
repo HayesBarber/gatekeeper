912519a36f818df6b2801278714f84a1e899c57d
HayesBarber <barberhayes@gmail.com>
2026-01-11 21:59:13 -0500

    auto commit

--- apps/cli/lib/src/services/registery.dart
+++ apps/cli/lib/src/services/registery.dart
@@ -1,10 +1,20 @@
 import 'package:gatekeeper_cli/src/services/config_service.dart';
+import 'package:mason_logger/mason_logger.dart';
 
 class Registery {
-  Registery._();
-  static final _instance = Registery._();
+  Registery._(this._logger);
+  static Registery? _instance;
 
-  static Registery get I => _instance;
+  static Registery get I {
+    if (_instance == null) {
+      throw StateError(
+        'Registery not initialized',
+      );
+    }
+    return _instance!;
+  }
+
+  final Logger _logger;
 
   ConfigService? _configService;
   ConfigService get configService {
