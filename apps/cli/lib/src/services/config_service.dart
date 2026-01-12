3c09b8de72fdf5d1dec9cc4d052628b45c570a95
HayesBarber <barberhayes@gmail.com>
2026-01-11 21:46:55 -0500

    init config service

---
+++ apps/cli/lib/src/services/config_service.dart
@@ -0,0 +1,24 @@
+import 'dart:convert';
+
+import 'package:gatekeeper_cli/src/models/cli_config.dart';
+import 'package:gatekeeper_cli/src/utils/file_utils.dart';
+
+class ConfigService {
+  CliConfig? _config;
+
+  Future<CliConfig> loadCliConfig() async {
+    if (_config != null) {
+      return _config!;
+    }
+    try {
+      final content = await FileUtils.readFileAsString(
+        FileUtils.resolvePath('~/.gatekeeper/config.json'),
+      );
+      final jsonData = jsonDecode(content) as Map<String, dynamic>;
+      _config = CliConfig.fromJson(jsonData);
+      return _config!;
+    } catch (e) {
+      throw Exception('Failed to load CLI configuration: $e');
+    }
+  }
+}
