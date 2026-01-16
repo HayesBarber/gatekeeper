35a43974cdc8aa84cae68576bd2e8e114660ccd8
Hayes Barber <barberhayes@gmail.com>
2026-01-16 18:43:06 -0500

    remove logger from core

--- packages/gatekeeper_core/lib/gatekeeper_core.dart
+++ packages/gatekeeper_core/lib/gatekeeper_core.dart
@@ -1,10 +1,6 @@
 /// gatekeeper_core
 library;
 
-export 'src/constants/headers.dart';
-export 'src/constants/subdomains.dart';
 export 'src/dto/challenge_response.dart';
 export 'src/dto/challenge_verification_request.dart';
 export 'src/dto/challenge_verification_response.dart';
-export 'src/logging/logger.dart';
-export 'src/logging/wide_event.dart';
