e2d982fb59c51596f919fef34d21248549ccb940
Hayes Barber <barberhayes@gmail.com>
2026-01-16 18:59:23 -0500

    rename to gatekeeper dto

--- packages/gatekeeper_core/lib/gatekeeper_core.dart
+++
@@ -1,6 +0,0 @@
-/// gatekeeper_core
-library;
-
-export 'src/dto/challenge_response.dart';
-export 'src/dto/challenge_verification_request.dart';
-export 'src/dto/challenge_verification_response.dart';
